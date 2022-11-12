944713f562a8ece4a31a3887474dadea18f2718f
Alejandro Aguilera garcia <aaguiler@c3r5s3.42malaga.com>
2022-11-12 11:31:34 +0100

    Add ex00

---
+++ ex00/main.cpp
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aaguiler <aaguiler@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/12 11:10:20 by aaguiler          #+#    #+#             */
+/*   Updated: 2022/11/12 11:29:06 by aaguiler         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Dog.hpp"
+#include "Cat.hpp"
+#include "Animal.hpp"
+#include "WrongAnimal.hpp"
+#include "WrongCat.hpp"
+
+int main()
+{
+	const Animal* meta = new Animal();
+	const Animal* j = new Dog();
+	const Animal* i = new Cat();
+
+	std::cout << j->getType() << " " << std::endl;
+	std::cout << i->getType() << " " << std::endl;
+	i->makeSound(); //will output the cat sound!
+	j->makeSound();
+	meta->makeSound();
+
+	delete meta;
+	delete j;
+	delete i;
+
+	const WrongAnimal* metaWrong = new WrongAnimal();
+	const WrongAnimal* k = new WrongCat();
+
+	std::cout << k->getType() << " " << std::endl;
+	k->makeSound(); //will output the animal sound!
+	metaWrong->makeSound();
+
+	delete metaWrong;
+	delete k;
+	return 0;
+}
